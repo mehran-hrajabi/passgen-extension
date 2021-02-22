1b47d230ee3a614f7b5fb400d56d37180a325809
Mehran <mehran.hrajabi98@gmail.com>
2021-02-23 02:17:09 +0330

    Add error feedback

--- assets/script/popup.js
+++ assets/script/popup.js
@@ -4,18 +4,66 @@ const passwordGenerator = document.querySelector("#passGen");
 const nameInput = document.querySelector("#nameInput");
 const outputHash = document.querySelector("#outputHash");
 const copyImg = document.querySelector("#copy-to-clipboard");
-let algo;
+const masterPassError = document.querySelector("#masterError");
+const hashAlgoError = document.querySelector("#hashError");
+const doubleError = document.querySelector("#twoErrors");
+let algo, master;
 
 chrome.storage.sync.get(['master','algorithm'], function(vars){
     showMasterPass.textContent = vars.master;
     showHashAlgo.textContent = vars.algorithm;
     algo = vars.algorithm;
+    master = vars.master;
 });
 
 passwordGenerator.addEventListener("click",async function(){
-    let concatStrings = showMasterPass.textContent.concat(nameInput.value);
-    const digestHex = await digestMessage(concatStrings);
-    outputHash.value = digestHex;
+    if(master == undefined && algo != undefined){
+        masterPassError.classList.remove("remove");
+        masterPassError.classList.add("create");
+
+        setTimeout(function(){
+            masterPassError.classList.add("fade");
+        },2000);
+
+        setTimeout(function(){
+            masterPassError.classList.remove("create");
+            masterPassError.classList.add("remove");
+            masterPassError.classList.remove("fade");
+        },2500);
+    }
+    else if(algo == undefined && master != undefined){
+        hashAlgoError.classList.remove("remove");
+        hashAlgoError.classList.add("create");
+
+        setTimeout(function(){
+            hashAlgoError.classList.add("fade");
+        },2000);
+
+        setTimeout(function(){
+            hashAlgoError.classList.remove("create");
+            hashAlgoError.classList.add("remove");
+            hashAlgoError.classList.remove("fade");
+        },2500);
+    }
+    else if(algo == undefined && master == undefined){
+        doubleError.classList.remove("remove");
+        doubleError.classList.add("create");
+
+        setTimeout(function(){
+            doubleError.classList.add("fade");
+        },3500);
+
+        setTimeout(function(){
+            doubleError.classList.remove("create");
+            doubleError.classList.add("remove");
+            doubleError.classList.remove("fade");
+        },5000);
+    }
+    else{
+        let concatStrings = showMasterPass.textContent.concat(nameInput.value);
+        const digestHex = await digestMessage(concatStrings);
+        outputHash.value = digestHex;        
+    }
 });
 
 async function digestMessage(message) {
